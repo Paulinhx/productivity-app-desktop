4c9a51222324c00be03016ba17fa6ce70a2e7a0a
Paul <paul@machine26.com>
2023-01-09 21:45:20 +0100

    redo index and app

--- pages/_app.js
+++ pages/_app.js
@@ -4,29 +4,28 @@ import PomodoroTimer from '../components/PomodoroTimer';
 import Calendar from '../components/Calendar';
 import styles from '../styles/globals.css';
 
-
-
 function App() {
-  const [selectedEvent, setSelectedEvent] = useState(null);
-  const [todos, setTodos] = useState([]);
+const [selectedEvent, setSelectedEvent] = useState(null);
+const [todos, setTodos] = useState([]);
 
-  function handleEventSelect(event) {
-    setSelectedEvent(event);
-    // start the pomodoro timer
-    // add a todo to the todo list
-    setTodos([...todos, { text: event.title }]);
-  }
+function handleEventSelect(event) {
+setSelectedEvent(event);
+// start the pomodoro timer
+// add a todo to the todo list
+setTodos([...todos, { text: event.title }]);
+}
 
-  return (
-    <div>
-      <Calendar onEventSelect={handleEventSelect} />
-      <TodoList todos={todos} />
-      <PomodoroTimer />
-    </div>
-  );
+return (
+<div>
+<Calendar onEventSelect={handleEventSelect} />
+<TodoList todos={todos} />
+<PomodoroTimer />
+</div>
+);
 }
 
 export default App;
 
-//TODO add the following API set Up and Calendar from Notion to your component and configure your component accordingly to your needs for your application to work properly with your components that require these  features to be configured  // correct the index and app files accordingly
-//TODO add the electron desktop to your application      
+//TODO add the following API set Up and Calendar from Notion to your component and configure your component accordingly to your needs for your application to work properly with your components that require these features to be configured
+//TODO correct the index and app files accordingly
+//TODO add the electron desktop to your application
--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,26 @@
+index.js
+
 import Head from 'next/head';
 import { Inter } from '@next/font/google';
 import styles from '../styles/Home.module.css';
-
-
+import App from './_app';
 
 export default function Home() {
-return (
-<>
-<Head>
-<title>Productivity App</title>
-<meta name="viewport" content="width=device-width, initial-scale=1" />
-<link rel="icon" href="/favicon.ico" />
-</Head>
-<main className={styles.main}>
-<div className={styles.container}>
-<h1>Productivity App</h1>
-<TodoList />
-<PomodoroTimer />
-<Calendar onEventSelect={handleEventSelect} />
-</div>
-</main>
-</>
-);
+  return (
+    <>
+      <Head>
+        <title>Productivity App</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={styles.main}>
+        <div className={styles.container}>
+          <h1 className={inter.className}>Productivity App</h1>
+          <App />
+        </div>
+      </main>
+    </>
+  );
 }
-
-
 
 
